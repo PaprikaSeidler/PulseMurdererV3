984b2e381fe5982d637fa6fa6b2f01691ced6262
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2025-05-05 13:35:40 +0200

    Implemented ToString

--- PulseMurdererV3/Player.cs
+++ PulseMurdererV3/Player.cs
@@ -10,7 +10,7 @@
 
         public override string ToString()
         {
-            return "";
+            return $"ID: {Id}, Name: {Name}, Avatar: {Avatar}, IsMurderer: {IsMurderer}";
         }
     }
 }
