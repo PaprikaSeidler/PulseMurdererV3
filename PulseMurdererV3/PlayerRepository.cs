6a56040d3f10d8f4562e7b7a32bbef81bda4a407
Paprika Seidler <pas002@edu.zealand.dk>
2025-05-08 11:35:56 +0200

    Added AddPlayer()

--- PulseMurdererV3/PlayerRepository.cs
+++ PulseMurdererV3/PlayerRepository.cs
@@ -8,7 +8,8 @@ namespace PulseMurdererV3
 {
     public class PlayerRepository
     {
-        private List<Player> players = new();
+        private List<Player> players = new List<Player>();
+        private int _nextId = 1;
 
         public PlayerRepository(){
             players.Add(new Player() {Name = "Miki", IsMurderer = false});
@@ -20,7 +21,15 @@ namespace PulseMurdererV3
 
         public List<Player> GetAllPlayers()
         {
-            return new List<Player>(players);
+            var playerList = new List<Player>(players);
+            return playerList;
+        }
+
+        public Player AddPlayer(Player player)
+        {
+            player.Id = _nextId++;
+            players.Add(player);
+            return player;
         }
     }
 }
