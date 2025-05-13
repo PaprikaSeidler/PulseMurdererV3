20c9fcef2f25a3dbed5371ccda61cc4b7bd90c1b
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2025-05-13 12:03:07 +0200

    Updated backend for voting

---
+++ PulseMurdererV3/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PulseMurdereV3{
+    public class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Det her er ne test");
+        }
+    }
+}
